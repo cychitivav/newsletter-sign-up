861c1ee7f61dc97061cf39d23802fd2e5b05f38c
Cristian Chitiva <cychitivav@unal.edu.co>
2023-11-20 23:39:49 -0500

    Update CSS and add JavaScript for form submission

---
+++ js/sendForm.js
@@ -0,0 +1,9 @@
+document.querySelector("form").addEventListener("submit", (event) => {
+	event.preventDefault();
+	const data = new FormData(event.target);
+
+	document.getElementById("sent-email").innerHTML = data.get("email");
+
+	document.getElementById("home").style.display = "none";
+	document.getElementById("success").style.display = "block";
+});
